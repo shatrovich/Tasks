c0202e62f611014394ce66db48ee47d2c8c8a645
Nikita Shatrov <shatrovich@protonmail.com>
2019-11-04 04:04:13 +0300

    fix bugs and update tests

--- JavaScript/fibbee/index.ts
+++ JavaScript/fibbee/index.ts
@@ -3,7 +3,7 @@ type C = {
     y: number;
 }
 
-export const calculate = (size: number, rc: C, ec: C, count: number, value = 0): number => {
+export const calculate = (size: number, rc: C, ec: C, count: number): number => {
     if (rc.x === ec.x && rc.y === ec.y) {
         return 1;
     }
@@ -11,30 +11,30 @@ export const calculate = (size: number, rc: C, ec: C, count: number, value = 0):
     if (count === 0) return 0;
 
     if (rc.x === 1 && rc.y === 1) {
-        return calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1, value);
+        return calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1);
     }
 
     if (rc.x === size && rc.y === size) {
-        return calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1, value);
+        return calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1);
     }
 
     if (rc.x === 1) {
-        return calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1, value);;
+        return calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1);;
     }
 
     if (rc.y === 1) {
-        return calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1, value) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value);;
+        return calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1);;
     }
 
     if (rc.x === size) {
-        return calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1, value);;
+        return calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1);;
     }
 
     if (rc.y === size) {
-        return calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1, value) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value);;
+        return calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1);;
     }
 
-    return calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1, value) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1, value);;
+    return calculate(size, { x: rc.x - 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x + 1, y: rc.y }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y - 1 }, ec, count - 1) + calculate(size, { x: rc.x, y: rc.y + 1 }, ec, count - 1);;
 }
 
 export default calculate;
--- JavaScript/fibbee/test.js
+++ JavaScript/fibbee/test.js
@@ -1,23 +1,19 @@
 const { calculate } = require("./index");
 
 describe("calculate", () => {
-    it("someOne", () => {
+    it("100 { x: 1, y: 1 } { x: 2, y: 2 } 2", () => {
         expect(calculate(100, { x: 1, y: 1 }, { x: 2, y: 2 }, 2)).toBe(2);
     });
 
-    it("someThreee", () => {
+    it("100 { x: 1, y: 1 } { x: 2, y: 2 } 1", () => {
         expect(calculate(100, { x: 1, y: 1 }, { x: 2, y: 2 }, 1)).toBe(0);
     });
 
-    it("someThree", () => {
-        expect(calculate(100, { x: 1, y: 1 }, { x: 2, y: 2 }, 4)).toBe(8);
+    it("100 { x: 1, y: 1 } { x: 2, y: 2 } 4", () => {
+        expect(calculate(100, { x: 1, y: 1 }, { x: 2, y: 2 }, 4)).toBe(10);
     });
 
-    // it("someTwo", () => {
-    //     expect(calculate(100, { x: 1, y: 1 }, { x: 5, y: 5 }, 8)).toBe(70);
-    // })
-
-    it("some", () => {
+    it("100 { x: 1, y: 1 } { x: 5, y: 5 } 10", () => {
         expect(calculate(100, { x: 1, y: 1 }, { x: 5, y: 5 }, 10)).toBe(2520);
     })
 })
